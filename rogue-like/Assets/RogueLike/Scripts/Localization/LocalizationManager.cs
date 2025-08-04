6ac470b4e8d03d25a815e175d38c58abb47ce823
oort-games <oortgamestudio@gmail.com>
2025-08-04 21:02:13 +0900

    feat(ui): 번역 테이블 적용

--- rogue-like/Assets/RogueLike/Scripts/Localization/LocalizationManager.cs
+++ rogue-like/Assets/RogueLike/Scripts/Localization/LocalizationManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LocalizationManagerEx : MonoBehaviour
+public class LocalizationManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
