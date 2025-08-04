9c15f7cef4f023e8373f6d12832b9dd48c976348
oort-games <oortgamestudio@gmail.com>
2025-08-05 00:58:09 +0900

    feat(display): 해상도 변경 로직 중복 호출 제거

--- rogue-like/Assets/RogueLike/Scripts/Display/DisplayManager.cs
+++ rogue-like/Assets/RogueLike/Scripts/Display/DisplayManager.cs
@@ -1,9 +1,10 @@
+using System.Collections;
 using UnityEngine;
 
 public class DisplayManager : Manager<DisplayManager>
 {
-    const string KEY_RESOLUTION = "Display.Resolution";
     const string KEY_SCREENMODE = "Display.ScreenMode";
+    const string KEY_RESOLUTION = "Display.Resolution";
     const string KEY_TARGETFRAMERATE = "Display.TargetFrameRate";
     const string KEY_LIMITFRAMERATE = "Display.LimitFrameRate";
     const string KEY_VSYNC = "Display.VSync";
@@ -14,16 +15,18 @@ public class DisplayManager : Manager<DisplayManager>
     bool _limitFrameRate;
     bool _vSync;
 
-    public DisplayResolution GetResolution() => _resolution;
     public DisplayScreenMode GetScreenMode() => _screenMode;
+    public DisplayResolution GetResolution() => _resolution;
     public DisplayTargetFrameRate GetTargetFrameRate() => _targetFrameRate;
     public bool GetLimitFrameRate() => _limitFrameRate;
     public bool GetVSync() => _vSync;
 
+    bool _isDisplayResoultionChanging = false;
+
     public override void Initialize()
     {
 #if UNITY_STANDALONE
-        _screenMode = (DisplayScreenMode)PlayerPrefs.GetInt(KEY_SCREENMODE, (int)DisplayScreenMode.FullScreen);
+        _screenMode = (DisplayScreenMode)PlayerPrefs.GetInt(KEY_SCREENMODE, (int)DisplayScreenMode.FullScreenWindow);
         _resolution = (DisplayResolution)PlayerPrefs.GetInt(KEY_RESOLUTION, (int)DisplayResolution.Resolution_1920x1080);
         _targetFrameRate = (DisplayTargetFrameRate)PlayerPrefs.GetInt(KEY_TARGETFRAMERATE, (int)DisplayTargetFrameRate.FPS_120);
         _limitFrameRate = PlayerPrefs.GetInt(KEY_LIMITFRAMERATE, 1) == 1;
@@ -44,18 +47,20 @@ public class DisplayManager : Manager<DisplayManager>
         SetVSync(false);
     }
 
-    public void SetResolution(DisplayResolution resolution)
-    {
-        _resolution = resolution;
-        ApplyDisplay();
-        SavePref(KEY_RESOLUTION, (int)_resolution);
-    }
-
     public void SetScreenMode(DisplayScreenMode screenMode)
     {
         _screenMode = screenMode;
-        ApplyDisplay();
+        if (_isDisplayResoultionChanging == false)
+            StartCoroutine(ApplyDisplayRoutine());
         SavePref(KEY_SCREENMODE, (int)_screenMode);
+    }
+
+    public void SetResolution(DisplayResolution resolution)
+    {
+        _resolution = resolution;
+        if (_isDisplayResoultionChanging == false)
+            StartCoroutine(ApplyDisplayRoutine());
+        SavePref(KEY_RESOLUTION, (int)_resolution);
     }
 
     public void SetTargetFrameRate(DisplayTargetFrameRate targetFrameRate)
@@ -81,9 +86,18 @@ public class DisplayManager : Manager<DisplayManager>
 
     public void ApplyDisplay()
     {
+        Debug.Log("ApplyDisplay");
         (int width, int height) = _resolution.ToSize();
         FullScreenMode fullScreenMode = _screenMode.ToFullScreenMode();
         Screen.SetResolution(width, height, fullScreenMode);
+    }
+
+    IEnumerator ApplyDisplayRoutine()
+    {
+        _isDisplayResoultionChanging = true;
+        yield return null;
+        ApplyDisplay();
+        _isDisplayResoultionChanging = false;
     }
 
     public void ApplyTargetFrameRate()
